91423492da223bf3214f34be03f9719d2e03a350
Agus Budi Santoso <agus.budi@pintap.id>
2023-02-08 21:33:03 +0700

    update env

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,11 @@
 import {Sequelize} from "sequelize";
 
 export const database = new Sequelize({
-  host: "pintapdb.crqrjxcidqof.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  database: "pintapdb",
-  username: 'pintap',
-  password: 'pintap123',
+  host: process.env.DB_HOST ?? '',
+  port: process.env.DB_PORT ? parseFloat(process.env.DB_PORT) : 3306,
+  database: process.env.DB_NAME ?? '',
+  username: process.env.DB_USERNAME ?? '',
+  password: process.env.DB_PASSWORD ?? '',
   dialect: "mariadb",
   storage: ":memory:",
 });
